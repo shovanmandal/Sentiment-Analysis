d4aef174516d338e345fac17cdf7526300e16e4d
shovanmandal <sushovan.93@gmail.com>
2017-05-29 18:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk import*
-sampleText=''
+sampleText='client is having an issue in running Bw reports,she has tried running a few different reports in BW today and the response time is EXTREMELY slow or unresponsive.'
 wordTokens=nltk.word_tokenize(sampleText)
 sentTokens=nltk.sent_tokenize(sampleText)
 
